faec3e9b58a7c8799b0a812a48558e5ddadd3bb5
NexusQuantum_ <shuukiuesugi1@gmail.com>
2024-11-14 09:48:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-let targets = document.querySelectorAll('.scrollAnimation');//アニメーションさせたい要素
-let offset = 100;//アニメーションタイミング
+let targets = document.querySelectorAll(".scrollAnimation"); //アニメーションさせたい要素
+let offset = 100; //アニメーションタイミング
 
-window.addEventListener('scroll', function() {//スクロールしたとき
+window.addEventListener("scroll", function () {
+  //スクロールしたとき
 
-  var scroll = window.scrollY;//スクロール量を取得
-  var h = window.innerHeight;//画面の高さを取得
+  var scroll = window.scrollY; //スクロール量を取得
+  var h = window.innerHeight; //画面の高さを取得
 
-  for(let target of targets) {
-    var pos = target.getBoundingClientRect().top + scroll;//アニメーションさせたい要素の位置を取得
-    if (scroll > pos - h + offset) {//スクロール量 > アニメーションさせたい要素の位置
-      target.classList.add('is-animated');
+  for (let target of targets) {
+    var pos = target.getBoundingClientRect().top + scroll; //アニメーションさせたい要素の位置を取得
+    if (scroll > pos - h + offset) {
+      //スクロール量 > アニメーションさせたい要素の位置
+      target.classList.add("is-animated");
     }
   }
-
 });
 
-window.onload = async function() {
-  const spinner = document.getElementById('loading');
- 
+window.onload = async function () {
+  const spinner = document.getElementById("loading");
+
   // Add .loaded to .loading
   await sleep(1000);
-  spinner.classList.add('loaded');
-}
+  spinner.classList.add("loaded");
+};
